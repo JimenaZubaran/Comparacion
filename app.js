0a6cdb6fdf9d03063315da7e9406dcb8d39a978f
JimenaZubaran <jimenab.zubaran@gmail.com>
2017-10-24 14:32:50 +0000

    Commit final

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var comparar = function (arreglo1, arreglo2){
-  if (arreglo1.length === arreglo2.length)
+var comparar = function (arreglo1, arreglo2)
+{
+  if (arreglo1.length !== arreglo2.length)
   {
-    document.write ("si son iguales");
+  / return false;
+  } else
+{
+    for (var i = 0;i < arreglo1.length; i ++)
+  {
+       if(arreglo1[i]!==arreglo2[i])
+     {
+        return false;
+      }
+    }
+      return true;
   }
 }
 
- var array1 = [1,2,3,4,5,6,7];
+ var array1 = [1,2,3,4,5,8,7];
  var array2 = [1,2,3,4,5,6,7];
 
  comparar (array1, array2);
